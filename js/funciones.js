09fc8604b83187485af777a7977a6e20fa0fdcb9
SchatzM <bri@npass.us>
2020-05-27 22:34:53 +0200

    Añadida clase principal

--- js/funciones.js
+++ js/funciones.js
@@ -1,1 +1,33 @@
 'use strict';
+/**
+ * Práctica 10
+ * Clases y Herencias
+ * Brian Passos
+ */
+
+ // Clase principal
+class Microprocesador {
+	constructor (
+		fabricante = 'Desconocido',
+		númeroNúcleos = 0,
+		númeroHilos = 0,
+		velocidadBase = 0,
+		TDP = 0
+	) {
+		this.fabricante = fabricante;
+		this.númeroNúcleos = númeroNúcleos;
+		this.númeroHilos = númeroHilos;
+		this.velocidadBase = velocidadBase;
+		this.TDP = TDP;
+	};
+
+	// Primer método que devuelve un resumen con lo datos proporcionados
+	resumen () {
+		return	`Microprocesador ${this.fabricante} ${this.família}™ ${this.modelo}\n`+
+				`========================================\n`+
+				`Cantidad de núcleos:\t\t\t${this.númeroNúcleos}\n`+
+				`Cantidad de hilos:\t\t\t\t${this.númeroHilos}\n`+
+				`Frecuencia base del procesador:\t${this.velocidadBase} GHz\n`+
+				`TDP:\t\t\t\t\t\t\t${this.TDP} W\n\n`;
+	};
+};
